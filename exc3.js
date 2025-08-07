c05e60a811f30ee427f88d3a6e3c61f2d357adec
NonieM <getitdone186@gmail.com>
2025-08-07 14:00:22 +0200

    added task

---
+++ exc3.js
@@ -0,0 +1,12 @@
+function price(item1, item2, item3) {
+  return item1 + item2 + item3;
+}
+// This function calculates the total price of three items
+
+var total = price(10.99, 5.50, 3.25);
+
+// Output to console
+console.log("Total price is: R" + total);
+
+// Output to webpage
+document.getElementById("output").textContent = "Total price is: R" + total;
