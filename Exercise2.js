86d89740714e156249cfd1f5c1f1db0b85e8fa48
NonieM <getitdone186@gmail.com>
2025-08-07 13:21:17 +0200

    new file:   Exercise2.js

---
+++ Exercise2.js
@@ -0,0 +1,11 @@
+// Step 1: Create a variable and assign it the value 2
+var chapterNumber = 2;
+
+// Step 2: Create a second variable with the string
+var message = "This is chapter number: ";
+
+// Step 3: Concatenate the string and number
+var fullMessage = message + chapterNumber;
+
+// Step 4: Output it to the browser window
+document.write(fullMessage);
